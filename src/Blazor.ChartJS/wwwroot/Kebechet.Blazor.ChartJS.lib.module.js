8f58e3b4026101291cd3c09a594b22372199e2be
Samuel Sidor <samosidor@gmail.com>
2023-07-04 13:11:49 +0200

    fixed initializer

--- src/Blazor.ChartJS/wwwroot/Kebechet.Blazor.ChartJS.lib.module.js
+++ src/Blazor.ChartJS/wwwroot/Kebechet.Blazor.ChartJS.lib.module.js
@@ -2,12 +2,12 @@
     console.log("Injecting Chart.js");
 
     var element = document.createElement('script');
-    element.src = "_content/Blazor.ChartJS/Chart.js";
+    element.src = "_content/Kebechet.Blazor.ChartJS/Chart.js";
     element.async = true;
     document.body.appendChild(element);
 
     var element2 = document.createElement('script');
-    element2.src = "_content/Blazor.ChartJS/ChartInterop.js";
+    element2.src = "_content/Kebechet.Blazor.ChartJS/ChartInterop.js";
     element2.async = true;
     document.body.appendChild(element2);
 }
